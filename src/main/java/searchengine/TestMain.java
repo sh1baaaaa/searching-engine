3a0f2f98a72c92fdd8275b2d881dbd4f32fd7e53
daniil_iagodkin <tehnokot2008@gmail.com>
2024-12-06 13:52:13 +1000

    start classes

---
+++ src/main/java/searchengine/TestMain.java
@@ -0,0 +1,19 @@
+package searchengine;
+
+
+import org.springframework.stereotype.Service;
+
+
+import java.io.IOException;
+
+@Service
+public class TestMain {
+
+
+    public static void main(String[] args) throws IOException {
+
+    }
+
+
+
+}

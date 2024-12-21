c5c1d2108c54940930a69f127cd54c41b00382ca
daniil_iagodkin <tehnokot2008@gmail.com>
2024-12-22 01:09:28 +1000

    SQL queries to find indexes by lemma and site and to count lemmas was added

--- src/main/java/searchengine/repository/IndexRepository.java
+++ src/main/java/searchengine/repository/IndexRepository.java
@@ -5,10 +5,34 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import searchengine.entity.IndexEntity;
 
+import java.util.List;
+
 @Repository
 public interface IndexRepository extends JpaRepository<IndexEntity, Integer> {
 
     Boolean existsByPagePathAndLemmaLemma(String pagePath, String lemma);
 
     void deleteByPagePathAndLemmaLemma(String pagePath, String lemma);
+
+    List<IndexEntity> findByLemmaLemma(String lemma);
+
+    @Query(nativeQuery = true
+            , value = "SELECT COUNT(*) FROM `index` i JOIN lemma l ON i.lemma_id = l.id WHERE l.lemma = :lemma")
+    Integer lemmaCount(String lemma);
+
+    @Query(nativeQuery = true
+            , value = "SELECT COUNT(*) FROM `index` i " +
+            "JOIN lemma l ON i.lemma_id = l.id " +
+            "JOIN site s ON l.site_id = s.id " +
+            "WHERE l.lemma = :lemma AND s.name = :site")
+    Integer lemmaCount(String lemma, String site);
+
+    @Query(nativeQuery = true,
+            value = "SELECT * FROM 'index' i " +
+                    "JOIN page p ON i.page_id = p.id " +
+                    "JOIN site s ON p.site_id = s.id " +
+                    "JOIN lemma l ON i.lemma_id = l.id " +
+                    "WHERE s.site = :site AND l.lemma = :lemma ")
+    List<IndexEntity> findByLemmaAndSite(String lemma, String site);
+
 }

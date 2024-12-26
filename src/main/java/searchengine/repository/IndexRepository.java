388fc035e459f3be52318a15b7061e63beb68602
daniil_iagodkin <tehnokot2008@gmail.com>
2024-12-26 23:37:32 +1000

    findByLemmaAndSite query was fixed

--- src/main/java/searchengine/repository/IndexRepository.java
+++ src/main/java/searchengine/repository/IndexRepository.java
@@ -12,8 +12,6 @@ public interface IndexRepository extends JpaRepository<IndexEntity, Integer> {
 
     Boolean existsByPagePathAndLemmaLemma(String pagePath, String lemma);
 
-    void deleteByPagePathAndLemmaLemma(String pagePath, String lemma);
-
     List<IndexEntity> findByLemmaLemma(String lemma);
 
     @Query(nativeQuery = true
@@ -28,11 +26,11 @@ public interface IndexRepository extends JpaRepository<IndexEntity, Integer> {
     Integer lemmaCount(String lemma, String site);
 
     @Query(nativeQuery = true,
-            value = "SELECT * FROM 'index' i " +
+            value = "SELECT * FROM `index` i " +
                     "JOIN page p ON i.page_id = p.id " +
                     "JOIN site s ON p.site_id = s.id " +
                     "JOIN lemma l ON i.lemma_id = l.id " +
-                    "WHERE s.site = :site AND l.lemma = :lemma ")
+                    "WHERE s.url = :site AND l.lemma = :lemma ")
     List<IndexEntity> findByLemmaAndSite(String lemma, String site);
 
 }
